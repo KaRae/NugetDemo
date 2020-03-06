afc2c95b9c98a283d1559b50df9fec95b92da216
Kay Raettig <kay.raettig@dnvgl.com>
2020-03-06 15:33:03 +0100

    Added the greeter

---
+++ Framework/MyLib/MyLib/Greeter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Greeter
+    {
+        public void Greet(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new Exception();
+
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+
+    }
+}
